cbb50b8b1c79a5a8a872a84a533449683b446e8c
AlbertMT10 <frankcoaquirajusto4@gmail.com>
2022-11-25 10:31:51 -0500

    expo FNAF

---
+++ prototype/Circle.java
@@ -0,0 +1,13 @@
+public class Circle extends Shape{
+    public Circle(){
+        type = "Square";
+    }
+
+
+    public void draw(){
+        System.out.println("Inside Square::draw() method. ");
+    
+    }
+    
+    
+}
